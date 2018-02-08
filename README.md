e5d22b2cf700fbddcf2a3f4e9f8f63c178010e22
antigones <antigones>
2018-02-08 20:29:34 +0100

    Initial version

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# hsk5_words_graph
+# HSK5 Word Graph
+
+Just a graph representation of the word list for HSK5
+
+hsk5_word_graph.py is a simple python script that builds an adjacency list suitable to be imported by Gephi.
+
+The word list (ordered by frequency) has been taken from http://www.hskhsk.com/word-lists.html
+
+
+
+![hsk5_full_view](hsk5_full_view.png)
+
+![Screenshot_2](Screenshot_2.png)
+
+
+
+This project has been deeply inspired by the work done in [http://www.hskhsk.com/graphs.html](http://www.hskhsk.com).
+
---
+++ hsk5_word_graph.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+import csv
+
+word_array = []
+file = open("output.csv","w",encoding='utf8') 
+
+with open('HSK5_freqorder.txt', 'r',encoding='utf8') as csvfile:
+    freader = csv.reader(csvfile, delimiter='\t')
+    for row in freader:
+        word_array.append(row[0])
+
+hmap = {}
+for i in range(0,len(word_array)):
+    # for each word...
+    cur_word = word_array[i]
+    for j in range(0,len(cur_word)):
+        # for each word component
+        hanzi = cur_word[j]
+        for k in range(0,len(word_array)):
+            # search in every other word...
+            opp_word = word_array[k]
+            for w in range(0,len(opp_word)):
+                # a component in this other word...
+                opp_hanzi = opp_word[w]
+                if (i != k):
+                    if (opp_hanzi == hanzi):
+					# equal to the source word
+                        cur_values = hmap.get(hanzi,[])
+                        cur_values.append(opp_word)
+                        hmap[hanzi] = cur_values
+
+# use the map to write a csv to be used by Gephi
+for key, value in hmap.items():
+    file.write(key+","+','.join(hmap[key])+'\n')
+file.close()
